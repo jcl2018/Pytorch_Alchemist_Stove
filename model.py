221e1cdb2b2a33bc2a26025a9fe707155335041e
jcl2018 <42528632+jcl2018@users.noreply.github.com>
2023-05-28 19:10:16 -0700

    Session2: refactoring model classes

--- model.py
+++ model.py
@@ -1,22 +1,57 @@
 import torch.nn as nn
-from option import input_size, hidden_size, num_classes, device
 
-# Step3: define models #
-# Define the MLP model
-class MLP(nn.Module):
+from option import input_size, hidden_size, num_classes, device
+from abc import ABC, abstractmethod
+
+
+# Define the interface
+class ModelInterface(ABC, nn.Module):
+    @abstractmethod
+    def forward(self, x):
+        pass
+
+    @abstractmethod
+    def print_model(self):
+        pass
+
+
+class LayerInterface(ABC, nn.Module):
+    @abstractmethod
+    def forward(self, x):
+        pass
+
+class DenseLayer(LayerInterface):
+    def __init__(self, input_size, output_size, activation=None):
+        super(LayerInterface, self).__init__()
+        self.fc = nn.Linear(input_size, output_size)
+        self.act = activation
+
+    def forward(self, x):
+        x = self.fc(x)
+        if self.act:
+            x = self.act(x)
+        return x
+
+
+class MLPModel(ModelInterface):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(MLP, self).__init__()
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+        super(ModelInterface, self).__init__()
+
+        self.flatten = nn.Flatten().to(device)
+        self.dense1 = DenseLayer(input_size, hidden_size, activation=nn.ReLU()).to(device)
+        self.dense2 = DenseLayer(hidden_size, num_classes).to(device)
 
     def forward(self, x):
         x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
         return x
 
+    def print_model(self):
+        print(self.dense1)
+        print(self.dense2)
+
+
 # Initialize the model
-mlp_model = MLP(input_size, hidden_size, num_classes).to(device)
+mlp_model = MLPModel(input_size, hidden_size, num_classes)
+mlp_model.print_model()
